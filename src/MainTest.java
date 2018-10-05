b2555d6a15f4242ba309674d39d0f967ec4d6861
Alena Prideina <alena.prideina@yandex.ru>
2018-10-05 23:57:17 +0700

    example of work with static field

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,17 +2,30 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase
 {
-    MathHelper Math = new MathHelper();
-    int a = 10;
-    int b = 4;
+    @Test
+    public void myFirstTest() {
+        System.out.println("First test : Before changing static_int " + MathHelper.static_int);
+
+        MathHelper.static_int = 8;
+
+        MathHelper mathObject = new MathHelper();
+        System.out.println("First test : Before changing simple_int " + mathObject.simple_int);
+
+        mathObject.simple_int = 8;
+    }
 
     @Test
-    public void MyFirstTest(){
-        int a = Math.calc(this.a, this.b, '+');
-        System.out.println(a);
+    public void mySecondTest() {
+        System.out.println("Second test : Before changing static_int " + MathHelper.static_int);
 
-        int b = Math.calc(this.a, this.b, '*');
-        System.out.println(b);
+        MathHelper.static_int = 8;
+
+        MathHelper mathObject = new MathHelper();
+        System.out.println("Second test : Before changing simple_int " + mathObject.simple_int);
+
+        mathObject.simple_int = 8;
     }
+
+
 
 }
