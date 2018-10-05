5dbb366b97094c4a3a4dfcb76d98be4986b084c3
Alena Prideina <alena.prideina@yandex.ru>
2018-10-06 00:08:38 +0700

    delete MathHelper

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,28 +4,12 @@ public class MainTest extends CoreTestCase
 {
     @Test
     public void myFirstTest() {
-        System.out.println("First test : Before changing static_int " + MathHelper.static_int);
 
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("First test : Before changing simple_int " + mathObject.simple_int);
-
-        mathObject.simple_int = 8;
     }
 
     @Test
     public void mySecondTest() {
-        System.out.println("Second test : Before changing static_int " + MathHelper.static_int);
 
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("Second test : Before changing simple_int " + mathObject.simple_int);
-
-        mathObject.simple_int = 8;
     }
-
-
-
+    
 }
